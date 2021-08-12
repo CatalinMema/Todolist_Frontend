70c53ce67c13338cbd63109bf7efc0c92268b5b0
catalin.mema@titeica.com <catalin.mema@titeica.com>
2021-08-12 10:27:53 +0300

    feat: add date selector

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,60 +1,83 @@
 import React, { useState, useEffect, useRef } from 'react';
+import {ADD_TODO, UPDATE,UPDATE_TODO} from '../utils/constants';
 
-function TodoForm(props) {
-  const [input, setInput] = useState(props.edit ? props.edit.value : '');
-
+function TodoForm({edit,onChangeSetDate,onSubmit,dateOfTodo}) {
+  const [input, setInput] = useState(edit ? edit.value : '');
+  const [inputdate, setInputDate] = useState(dateOfTodo);
   const inputRef = useRef(null);
 
   useEffect(() => {
     inputRef.current.focus();
   });
-
+  
+ 
+   
   const handleChange = e => {
     setInput(e.target.value);
   };
 
+  const handleDateChange = e => {
+    setInputDate(e.target.value);
+    onChangeSetDate(e.target.value);
+  };
+  
   const handleSubmit = e => {
     e.preventDefault();
 
-    props.onSubmit({
-      todo_text: input
+    onSubmit({
+      todo_text: input,
+      date_todo: inputdate
     });
 //     axios.post("/todos",{
 //       "todo_text":input
 //  })
     setInput('');
   };
-
+  
   return (
     <form onSubmit={handleSubmit} className='todo-form'>
-      {props.edit ? (
+      {edit ? (
         <>
           <input
-            placeholder='Update your item'
+            placeholder={UPDATE_TODO}
             value={input}
             onChange={handleChange}
             name='text'
             ref={inputRef}
-            className='todo-input edit'
+            className='todo-input-update edit'
           />
           <button onClick={handleSubmit} className='todo-button edit'>
-            Update
+            {UPDATE}
           </button>
         </>
       ) : (
-        <>
+        <div className="add_todo_container">
+          <div>
           <input
-            placeholder='Add a todo'
+            placeholder={ADD_TODO}
             value={input}
             onChange={handleChange}
             name='text'
             className='todo-input'
             ref={inputRef}
           />
+          </div>
+          <div className="todo_buttons">
+          {/* <button className='todo-button'>
+            {DATE}
+          </button> */}
+          <input type="date" className='date_input'
+          value={inputdate}
+          onChange={handleDateChange}
+          
+       />
+
+          <br />
           <button onClick={handleSubmit} className='todo-button'>
-            Add todo
+            {ADD_TODO}
           </button>
-        </>
+          </div>
+        </div>
       )}
     </form>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,40 +1,129 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import TodoForm from './TodoForm';
-import Todo from './Todo';
 import axios from 'axios';
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
-import { Button } from '@material-ui/core';
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
+import {ADD_SOME, NEXT, NOTHING_PLANNED, PREV, SEARCH_TODO, SKIPS, TITLE, TODO_NOT_FOUND, UPDATE_TODO} from '../utils/constants';
 function TodoList() {
   const [todos, setTodos] = useState([]);
+  const [nr_skips,setnr_skips] = useState(0);
+  const [todosNumber,setTodosNumber]=useState(0);
+  const currentDate = new Date();
+  const currentDayOfMonth = currentDate.getDate();
+  const currentMonth = currentDate.getMonth(); // Be careful! January is 0, not 1
+  const currentYear = currentDate.getFullYear();
+  let dateString = undefined;
+  if(currentMonth < 10){
+    dateString =+ currentYear + "-" + 0 +(currentMonth + 1) + "-" +  currentDayOfMonth ;
+  }
+  else{
+    dateString=+ currentYear + "-" + (currentMonth + 1) + "-" +  currentDayOfMonth ;
+  }
+  const [dateOfTodo,setDateOfTodo]= useState(dateString)
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  });
+  const inputRef = useRef(null);
+
+  const onChangeSetDate = async todo =>{
+    setDateOfTodo(todo);
+    await axios.get(`/dayfilter/${todo}/8`).then(res => setTodos(res.data));
+  }
+  const [input, setInput] = useState('');
+    const [response,setResponse] = useState([]);
+    const handleChange = async e => {
+        setInput(e.target.value);
+        if(e.target.value)
+        {
+     await axios.get(`/search/${e.target.value}`).then(res => setResponse(res.data))
+        }
+        else{
+            setResponse([])
+        }
+    };
+    useEffect(() => {
+      inputRef?.current?.focus();
+    },[input]);
+  const submitUpdate = value => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
+
 
   const addTodo = async todo => {
     if (!todo.todo_text || /^\s*$/.test(todo.todo_text)) {
       return;
     }
-   await axios.post("/todos",{
-      "todo_text":todo.todo_text
-    })
-    await axios.get(`/getTodos`).then(res => setTodos(res.data)) 
-    };
+    await axios.post("/todos",{
+      "todo_text":todo.todo_text,
+      "date_todo":todo.date_todo
+    });
+    await axios.get(`/dayfilter/${todo.date_todo}/${nr_skips}`).then(res => setTodos(res.data));
+   // await axios.get(`/getTodos/page/${nr_skips}`).then(res => setTodos(res.data));
+    await axios.get(`/dayfilter/${dateOfTodo}`).then(res => setTodosNumber(res.data));
+  };
   
-    useEffect(()=>{
-    axios.get(`/getTodos`).then(res => setTodos(res.data))
-    },[])
-    console.log(todos)
-    const updateTodo = (todoId, newValue) => {
+  // useEffect(()=>{
+  //   async function fetchData() {
+  //     await axios.get(`/getTodos`).then(res => setTodosNumber(res.data))
+  //   };
+  //   fetchData();
+  //    },[todos.length])
+   
+  // useEffect(()=>{
+  //   async function fetchData() {
+  //     await axios.get(`/getTodos/page/${nr_skips}`).then(res => setTodos(res.data));
+  //   }
+  //   fetchData();
+  //   },[todos.length,nr_skips])
+
+   useEffect(()=>{
+    async function fetchData() {
+      await axios.get(`/dayfilter/${dateOfTodo}`).then(res => setTodosNumber(res.data))
+    };
+    fetchData();
+     },[dateOfTodo,todos.length])
+
+   useEffect(()=>{
+    async function fetchData() {
+      await axios.get(`/dayfilter/${dateOfTodo}/${nr_skips}`).then(res => setTodos(res.data));
+    }
+    fetchData();
+    },[todos.length,dateOfTodo,nr_skips])
+  
+  const updateTodo = async (todoId, newValue) => {
     if (!newValue.todo_text || /^\s*$/.test(newValue.todo_text)) {
       return;
     }
-    axios.post('/update',{"id":todoId,"todo_text":newValue.todo_text})
-    axios.get(`/getTodos`).then(res => setTodos(res.data)) 
-    };
-
-    const removeTodo = id => {
-    const removedArr = [...todos].filter(todo => todo._id !== id);
-    axios.post('/delete',{"id":id});
-    setTodos(removedArr);
+   await axios.post('/update',{"id":todoId,"todo_text":newValue.todo_text,"date_todo":newValue.date_todo})
+   await axios.get(`/dayfilter/${dateOfTodo}/${nr_skips}`).then(res => setTodos(res.data));
+   if(input){
+   await axios.get(`/search/${input}`).then(res => setResponse(res.data))
+   }
   };
 
+    const removeTodo = async id => {
+    const removedArr = [...todos].filter(todo => todo._id !== id);
+    await axios.post('/delete',{"id":id});
+    if(input){
+    await axios.get(`/search/${input}`).then(res => setResponse(res.data))}
+   //axios.get('/getTodos').then(res => setTodosNumber(res.data))
+    // console.log('todosNumber.length')
+    // console.log(todosNumber.length)
+    // console.log('nr_skips')
+    // console.log(nr_skips)
+
+    if(todosNumber.length>=SKIPS && todosNumber.length-1===nr_skips){
+      setnr_skips(nr_skips-SKIPS);
+    }
+    setTodos(removedArr);
+    //axios.get('/getTodos').then(res => setTodosNumber(res.data))
+  };
+  
   const completeTodo = id => {
     let updatedTodos = todos.map(todo => {
       if (todo._id === id) {
@@ -45,19 +134,159 @@ function TodoList() {
     setTodos(updatedTodos);
   };
 
+  const newPage = (direction) => {
+    if (direction === "next") {
+      setnr_skips(nr_skips + SKIPS);
+    } else if (direction === "previous") {
+      setnr_skips(nr_skips - SKIPS);
+    }
+  };
+  //console.log(nr_skips);
+  
+  const todosList = todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div className="todo_text" key={todo._id} onClick={() => completeTodo(todo._id)}>
+        {todo.todo_text}
+      </div>
+      <div className='icons'>
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo._id)}
+          className='delete-icon'
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo._id, value: todo.todo_text })}
+          className='edit-icon'
+        />
+      </div>
+    </div>
+  ));
+
+  const SearchtodosList = response?.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div className="todo_text" key={todo._id} onClick={() => completeTodo(todo._id)}>
+        {todo.todo_text}
+      </div>
+      <div className='icons'>
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo._id)}
+          className='delete-icon'
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo._id, value: todo.todo_text })}
+          className='edit-icon'
+        />
+      </div>
+    </div>
+  ));
+
+  if(edit.id) {
+    return (
+    <div style={{minHeight:'51.9em'}}>
+    <h1>{UPDATE_TODO}</h1>
+    <TodoForm onChangeSetDate={onChangeSetDate} dateOfTodo={dateOfTodo} edit={edit} onSubmit={submitUpdate} />
+    </div>
+    );
+  }
+  if(input) {
+    return (
+    <div style={{minHeight:'51.9em'}}>
+    <h1>{SEARCH_TODO}</h1>
+    <form className='todo-form'>
+          <input
+          autoComplete="off"
+            placeholder='Search todo'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputRef}
+            className='todo-input-search'
+          />
+    </form>
+    {SearchtodosList.length > 0 ? (<>{SearchtodosList}</>) : (
+      <div style={{paddingTop:'20em'}}>
+    <p>{TODO_NOT_FOUND}</p>
+    </div>
+    )}
+    </div>
+    
+    );
+  }
+
+  
   return (
     <>
-      <h1>What's the Plan for Today?</h1>
-      <TodoForm onSubmit={addTodo} />
-      <Todo
-        todos={todos}
-        completeTodo={completeTodo}
-        removeTodo={removeTodo}
-        updateTodo={updateTodo}
-      />
+      <h1>{TITLE}</h1>
+    <TodoForm dateOfTodo={dateOfTodo} onChangeSetDate={onChangeSetDate} onSubmit={addTodo} />
+      <div>
+        <form className='todo-form'>
+          <input
+          autoComplete="off"
+          placeholder={SEARCH_TODO}
+          value={input}
+          onChange={handleChange}
+          name='text'
+          ref={inputRef}
+          className='todo-input-search'
+          />
+    </form>
+  
+    </div>
+      <div style={{minHeight:'33em'}}> 
+      {todos.length === 0 ? ( 
+      <div style={{paddingTop:'10em'}}>
+        <h1>{NOTHING_PLANNED}</h1>
+        <p>{ADD_SOME}</p>
+    </div>) : (
+        <>{todosList}</>
+      ) }
+    
+      </div>
       <div className="navigation_buttons">
-      <button className="btn_next">Prev</button>
-      <button className="btn_next">Next</button>
+      {nr_skips >= SKIPS ? ( <button
+      className='todo-button'
+            onClick={()=>newPage("previous")}
+            //className="button__page"
+            style={{
+                minWidth: '7vw',
+                marginTop:'15px',
+                marginLeft:'26px',
+                marginBottom:'15px',
+                }}
+            >Prev 
+            </button>) : (<button
+      className='todo-button'
+            onClick={()=>newPage("previous")}
+            //className="button__page"
+            style={{
+                minWidth: '7vw',
+                marginTop:'15px',
+                marginLeft:'26px',
+                marginBottom:'15px',
+                opacity: '0'
+                }}
+            >{PREV} 
+            </button>) }
+     
+            {nr_skips <todosNumber.length-SKIPS ? (<button
+            style={{
+                //backgroundColor:'white',
+                marginTop:'15px',
+                marginRight:'26px',
+                marginBottom:'15px',
+                color:'white',
+                minWidth:'7vw',
+                     }}
+            onClick={()=>newPage("next")}
+            className='todo-button'
+            >{NEXT} 
+            </button>) : (null) }
+            
       </div>
      
 
